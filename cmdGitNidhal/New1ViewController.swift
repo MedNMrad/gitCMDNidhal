886e64c7e2331b04088dd6d091e8a54f44c4f9b4
Open Eyes Consulting <nidhal.mrad@openeyesconsulting.com>
2019-09-04 10:51:05 +0100

    the first commit

---
+++ cmdGitNidhal/New1ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  New1ViewController.swift
+//  cmdGitNidhal
+//
+//  Created by Open Eyes Consulting on 9/4/19.
+//  Copyright © 2019 Open Eyes Consulting. All rights reserved.
+// created to test git 
+
+import UIKit
+
+class New1ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func sum(a : Int) -> Int {
+        return a * 3
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ cmdGitNidhal/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  cmdGitNidhal
+//
+//  Created by Open Eyes Consulting on 9/4/19.
+//  Copyright © 2019 Open Eyes Consulting. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("none")
+    }
+
+
+}
+
